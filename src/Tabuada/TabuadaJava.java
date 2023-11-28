5be6976988a462b659743e4490f1449d8a942f72
FIXWER <138541312+FIXWER@users.noreply.github.com>
2023-11-28 00:52:29 -0300

    Initial commit

---
+++ src/Tabuada/TabuadaJava.java
@@ -0,0 +1,17 @@
+package Tabuada;
+
+public class TabuadaJava {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10 ; i++) {
+            System.out.println("Fazendo tabuada do número: "+i);
+            for (int j = 1; j <=10 ; j++) {
+                System.out.println(i +" x "+ j + " = " +i * j);
+
+            }
+
+        }
+    }
+}
+
+
+
